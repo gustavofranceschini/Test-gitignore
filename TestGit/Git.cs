4b123b7ceeb49b92d4dd3ac235096ba5878f17bc
Gustavo Franceschini <gustavo.araujo18@fatec.sp.gov.br>
2022-08-05 09:04:19 -0300

    Test file gitignore

---
+++ TestGit/Git.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TestGit
+{
+    class Git
+    {
+        public string Name { get; set; }
+        public int Idade { get; set; }  
+        
+        public Git()
+        {
+        }
+
+        public Git(string name,int idade)
+        {
+            Name = name;
+            Idade = idade;
+        }
+
+    }
+}
---
+++ TestGit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este programa tem como objetivo aprender o git-ignore");
+            Console.WriteLine();
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Git git = new Git(name, idade);
+
+            Console.WriteLine();
+
+            Console.WriteLine("information people:");
+            Console.WriteLine("Name: "+git.Name);
+            Console.WriteLine("Idade: "+git.Idade);
+        }
+    }
+}
